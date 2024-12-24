6d139508b8ad7869661e71236a3cd9260150a357
Gayathri chennapatnam <152840303+Chennapatnam-gayathri@users.noreply.github.com>
2024-12-24 21:02:13 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,15 @@
+public class function{
+    public static void fun(String s , int n){
+        for(int i=0;i<n;i++){
+            System.out.println(s);
+
+        }
+        
+    }
+    
+
+public static void main(String[] arg){
+    fun("gayathri",8);
+}
+
+}
